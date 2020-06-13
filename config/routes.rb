e1c00dcdd42343126803ace8fcd33a46d17a4183
rdGarcia28 <rafael546@gmail.com>
2020-06-12 21:45:59 -0300

    criacao de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+root "home#index"
   resources :estoques
   resources :clientes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
